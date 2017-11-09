8da41cbfb36d6151c259f4def8fff0a9ec692602
Dice <ChrisChangDice@163.com>
2017-11-09 16:43:22 +0800

    第一次上传测试
    2017-11-9 16:42:50  Github 学习中进行的测试联系

---
+++ argue.js
@@ -0,0 +1,17 @@
+        num = num.toString().split('').map(Number)
+                .reduce(function(product, value) {
+                    return value * product;
+                });
+        timeMult++;
+    }
+    
+    console.log(timeMult);
+}
+
+var num = 999;
+persistence(num);
+
+var firstName = 'Jake';
+var lastName = 'Rawr';
+console.log(`My name is ${firstName} ${lastName}`);
+// My name is Jake Rawr
